60d1b7738ab12826fb8f1ffde38dd6b442dca744
sahanahiremat2005-maker <sahanahiremat2005@gmail.com>
2026-04-13 05:50:29 -0700

    Update train.py

--- train.py
+++ train.py
@@ -1,82 +1,29 @@
 import pandas as pd
 import joblib
-
 from sklearn.model_selection import train_test_split
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from sklearn.metrics import r2_score, mean_absolute_error
-
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBRegressor
 
 # Load dataset
-df = pd.read_csv("data/insurance.csv")
+df = pd.read_csv("insurance.csv")
 
-# Features and target
-X = df.drop("charges", axis=1)
+# Encoding
+df["sex"] = df["sex"].map({"male": 1, "female": 0})
+df["smoker"] = df["smoker"].map({"yes": 1, "no": 0})
+df["region"] = df["region"].astype("category").cat.codes
+
+# Feature Engineering
+df["bmi_category"] = pd.cut(df["bmi"], bins=[0,18.5,25,30,100], labels=[0,1,2,3]).astype(int)
+df["risk_score"] = df["age"]*0.2 + df["bmi"]*0.3 + df["smoker"]*5
+df["bmi_smoker"] = df["bmi"] * df["smoker"]
+
+X = df[["age","bmi","children","sex","smoker","region","bmi_category","risk_score","bmi_smoker"]]
 y = df["charges"]
 
-# Columns
-num_cols = ["age", "bmi", "children"]
-cat_cols = ["sex", "smoker", "region"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Preprocessing
-preprocessor = ColumnTransformer([
-    ("num", StandardScaler(), num_cols),
-    ("cat", OneHotEncoder(handle_unknown="ignore"), cat_cols)
-])
+model = XGBRegressor(n_estimators=150)
+model.fit(X_train, y_train)
 
-# Models
-models = {
-    "Linear Regression": LinearRegression(),
-    "Random Forest": RandomForestRegressor(
-        n_estimators=200,
-        random_state=42
-    ),
-    "XGBoost": XGBRegressor(
-        n_estimators=300,
-        max_depth=4,
-        learning_rate=0.05,
-        random_state=42
-    )
-}
+joblib.dump(model, "model.pkl")
 
-# Split data
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
-
-best_score = -1
-best_model = None
-best_name = ""
-
-# Train and compare
-for name, algo in models.items():
-    pipeline = Pipeline([
-        ("prep", preprocessor),
-        ("model", algo)
-    ])
-
-    pipeline.fit(X_train, y_train)
-    pred = pipeline.predict(X_test)
-
-    score = r2_score(y_test, pred)
-    mae = mean_absolute_error(y_test, pred)
-
-    print(f"{name}")
-    print(f"R2 Score: {score:.4f}")
-    print(f"MAE: {mae:.2f}")
-    print("-" * 30)
-
-    if score > best_score:
-        best_score = score
-        best_model = pipeline
-        best_name = name
-
-# Save best model
-joblib.dump(best_model, "models/model.pkl")
-
-print(f"Best Model: {best_name}")
-print("Model saved to models/model.pkl")
+print("Model trained using REAL dataset!")
